cb21bb9daec12fead529aac9f1be73f38f6146e6
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-07-02 14:23:41 +0700

    update

--- js/main.js
+++ js/main.js
@@ -35,3 +35,125 @@ $$('.header-item').forEach((item) => {
         }
     };
 });
+
+// HANDLE SHOW MODAL MODAL
+
+let currentVideoID = 0;
+
+const videoContent = [
+    {
+        title: "Bước 1. Đăng ký tài khoản",
+        urlVideo:"https://www.youtube.com/embed/pFLl-4zz0AE"
+    },
+    {
+        title: "Bước 2. Đăng nhập tài khoản",
+        urlVideo:"https://www.youtube.com/embed/JjOI_dSumAQ"
+    },
+    {
+        title: "Bước 3. Chuyển điểm vào tài khoản",
+        urlVideo:"https://www.youtube.com/embed/ZJ6Z0ZloH9g"
+    },
+    {
+        title: "Bước 4. Cách nạp và rút",
+        urlVideo:"https://www.youtube.com/embed/HHQrklSmLW8"
+    },
+    {
+        title: "Bước 5. Cách tra cứu số may mắn",
+        urlVideo:"https://www.youtube.com/embed/0Kio6qdElTg"
+    },
+    {
+        title: "Bước 6. Mua vé",
+        urlVideo:"https://www.youtube.com/embed/hk5KVY8jfVo"
+    },
+    {
+        title: "Bước 7. Đổi mật khẩu (nếu cần)",
+        urlVideo:"https://www.youtube.com/embed/zbMe4aW382Q"
+    }
+    
+]
+
+const playVideoWithID = (id) => {
+    currentVideoID = id
+
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+        keyboard: false
+    })
+    renderContentVideo(id)
+}
+
+const renderContentVideo = (id) => {
+    currentVideoID = parseInt(id)
+    
+    let videoData = videoContent[currentVideoID];
+
+    const controlHTML = videoContent.map((item,index) => {
+        let temp =""
+        if(index==currentVideoID){
+            temp = "active"
+        }
+        return (
+            `
+                <div class="video-content-item ${temp}" onclick="renderContentVideo(${index})">
+                    ${index+1}
+                </div>
+            `
+        )
+    }).join("")
+
+    console.log(currentVideoID)
+
+    $('.modal-content').innerHTML = (
+        `
+            <div class="modal-control">
+                <div class="modal-control-item" onclick="prevVideo()">
+                    <i class="fa-solid fa-chevron-left"></i>
+                </div>
+                <div class="modal-control-item" onclick="nextVideo()">
+                    <i class="fa-solid fa-chevron-right"></i>
+                </div>
+            </div>
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                    ${videoData.title}
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body centering">
+                <div class="video-content">
+                    <iframe width="100%" src="${videoData.urlVideo}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="video-content-list d-flex">
+                    
+                    ${controlHTML}
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    Đóng
+                </button>
+            </div> 
+        `
+    )
+}
+
+const nextVideo = () => {
+    if(currentVideoID<videoContent.length-1){
+        renderContentVideo(currentVideoID+1)
+    }else{
+        renderContentVideo(0)
+    }
+}
+
+const prevVideo = () => {
+    if(currentVideoID>0){
+        renderContentVideo(currentVideoID-1)
+    }else{
+        renderContentVideo(videoContent.length-1)
+    }
+}
+
+$$('.video-icon').forEach((item) => {
+    item.onclick = () => {
+        playVideoWithID(item.getAttribute("data-video"))
+    }
+});
